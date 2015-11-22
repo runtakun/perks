8deb5c4cbe102698b50b8bdb598c304d17b1ed65
Hiroyuki Shigeta <shigeta@glgl.org>
2015-11-22 13:17:23 +0900

    add initial implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
-var browserify = require('browserify');
 var gulp = require('gulp');
+var browserify = require('browserify');
+var runSequence = require('run-sequence');
 var source = require('vinyl-source-stream');
+
+gulp.task('watch', function() {
+    gulp.watch(['src/**/*.js', 'src/**/*.html', 'components/**/*.vue'], ['browserify']);
+});
 
 gulp.task('browserify', function() {
   return browserify('./src/main.js')
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./build/'));
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', function(){
+    runSequence('browserify', 'watch');
 });
